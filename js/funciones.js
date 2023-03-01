81f1c6ede825fdddc2451f9f33e4925999a4098e
Guillermo Casaña <guillermocasana@hotmail.com>
2023-03-01 18:30:11 -0300

    Primera vez usando Git

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+
+document.getElementById("Bt1").onclick = function () {
+    console.log("capturamos el elemento click");
+    document.getElementById("parrafo1").innerHTML = "ACINDAR SA";
+    document.getElementById("parrafo2").innerHTML = "Desempeño en funciones de operario metalúrgico. Control de calidad. Verificación de línea. Seguridad y Mantenimiento";
+}
+
+document.getElementById("Bt2").onclick = function () {
+    console.log("capturamos el elemento click");
+    document.getElementById("parrafo1").innerHTML = "TELECOMUNICACIONES BALA";
+    document.getElementById("parrafo2").innerHTML = "Tendido de líneas para grandes clientes. Conexión y Configuración del servicio de redes e internet";
+}
+
+document.getElementById("Bt3").onclick = function () {
+    console.log("capturamos el elemento click");
+    document.getElementById("parrafo1").innerHTML = "TRANSPORTE ALCE SA";
+    document.getElementById("parrafo2").innerHTML = "Conductor de Camiones y Flete interno";
+}
